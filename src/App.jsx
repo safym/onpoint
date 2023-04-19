5dc26be13a8d80bef5394dac0ee6ce601550268a
safym <osafym@gmail.com>
2023-04-19 12:13:18 +0300

    add normalize & sanitize css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import logo from './logo.svg'
 import './index.css'
+import 'sanitize.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Ghbdtn</h1>
+        <ul>
+          <li>fujjfdfdf</li>
+          <li>fujjfdfdf</li>
+          <li>fujjfdfdf</li>
+          <li>fujjfdfdf</li>
+        </ul>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
