28a197a929248cbde70330c63ac1fb0f599602c0
safym <osafym@gmail.com>
2023-04-20 23:12:07 +0300

    styles in progress

--- src/components/HorizontalContainer/index.jsx
+++ src/components/HorizontalContainer/index.jsx
@@ -1,14 +1,18 @@
 import React, { useRef, useEffect } from 'react'
-import Button from '../Button'
 
+import Button from '../Button'
 import Title from '../Title'
+
+import home from '../../assets/images/homeS.png'
 
 function HorizontalContainer() {
   const scrollRef = useRef(null)
+  const pagesCount = 3
+  const pages = Array.from(Array(pagesCount).keys())
 
   useEffect(() => {
     const container = scrollRef.current
-    const vw = window.innerWidth
+    container.style.transform = `translateX(0vw)`
 
     if (container) {
       const onWheel = (e) => {
@@ -16,18 +20,22 @@ function HorizontalContainer() {
 
         if (offsetY === 0) return
 
+        const transform = container.style.transform
+        const value = parseInt(
+          transform.substring(
+            transform.indexOf('(') + 1,
+            transform.indexOf('vw')
+          )
+        )
+
+        const currentPageNum = Math.abs(Math.trunc(value / 100))
+
         e.preventDefault()
 
         if (offsetY > 0) {
-          container.scrollTo({
-            left: container.scrollLeft + vw,
-            behavior: 'smooth',
-          })
+          moveToPage(currentPageNum + 1)
         } else {
-          container.scrollTo({
-            left: container.scrollLeft - vw,
-            behavior: 'smooth',
-          })
+          moveToPage(currentPageNum - 1)
         }
       }
       container.addEventListener('wheel', onWheel)
@@ -35,34 +43,51 @@ function HorizontalContainer() {
     }
   }, [])
 
-  const moveToSlide = (pageNum) => {
-    const container = scrollRef.current
-    const vw = window.innerWidth
+  const moveToPage = (pageNum) => {
+    if (!pages.includes(pageNum)) return
 
-    container.scrollTo({
-      left: pageNum * vw,
-      behavior: 'smooth',
-    })
+    const container = scrollRef.current
+
+    container.style.transform = `translateX(${-100 * pageNum}vw)`
   }
 
   return (
     <>
       <header className="header">
-        <button onClick={() => moveToSlide(0)}>home</button>
-        <button onClick={() => moveToSlide(1)}>PROJECT</button>
+        <nav>
+          <button
+            className="header__navlink navlink"
+            onClick={() => moveToPage(0)}
+          >
+            <img src={home} />
+          </button>
+          <button
+            className="header__navlink navlink"
+            onClick={() => moveToPage(1)}
+          >
+            project
+          </button>
+        </nav>
       </header>
-      <div className="container" ref={scrollRef}>
-        <div className="slide">
+
+      <div
+        className="container"
+        ref={scrollRef}
+        style={{ transform: 'translateX(0vw)' }}
+      >
+        <div className="page">
           <Title size="small">привет,</Title>
-          <Title size="small">
-            это не <br/>
-            коммерческое <br/>
+          <Title size="large">
+            это <span className="bold">не</span> <br />
+            коммерческое <br />
             задание
           </Title>
-          <Button>Что дальше?</Button>
+          <Button className="button" icon="arrow">
+            Что дальше?
+          </Button>
         </div>
-        <div className="slide">2</div>
-        <div className="slide">3</div>
+        <div className="page">2</div>
+        <div className="page">3</div>
       </div>
     </>
   )
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import './index.css'
+import './assets/styles/index.css'
 import App from './app/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
