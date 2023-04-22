8557a0448776cfbec24db59cd6638cb31da2df20
safym <osafym@gmail.com>
2023-04-22 17:24:25 +0300

    intermediate commit

--- src/components/ScrollableBlock/index.jsx
+++ src/components/ScrollableBlock/index.jsx
@@ -1,29 +1,11 @@
 import Card from '../Card'
 
-function ScrollableBlock({ text }) {
+function ScrollableBlock({ children, text }) {
   return (
     <Card className="scrollable-block">
       <div class="scrollable-block__content">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis ut
-          diam quam nulla. Mauris in aliquam sem fringilla ut morbi tincidunt.
-          Vitae aliquet nec ullamcorper sit amet risus nullam eget felis. Nulla
-          pharetra diam sit amet nisl. Eget nulla facilisi etiam dignissim diam
-          quis enim lobortis. Est sit amet facilisis magna. Neque laoreet
-          suspendisse interdum consectetur libero id. Nec ullamcorper sit amet
-          risus nullam eget felis eget. Mollis aliquam ut porttitor leo a diam
-          sollicitudin tempor id. Euismod quis viverra nibh cras pulvinar mattis
-          nunc. Massa massa ultricies mi quis. Sit amet massa vitae tortor
-          condimentum lacinia. Et malesuada fames ac turpis egestas integer
-          eget. Elementum pulvinar etiam non quam lacus suspendisse faucibus
-          interdum posuere. Amet justo donec enim diam vulputate ut pharetra
-          sit. Risus ultricies tristique nulla aliquet enim tortor at auctor.
-          Velit sed ullamcorper morbi tincidunt ornare massa. Quis hendrerit
-          dolor magna eget est lorem ipsum. Etiam dignissim diam quis enim.
-          Gravida neque convallis a cras. Ut enim blandit volutpat maecenas
-          volutpat. Mauris sit amet massa vitae tortor condimentum lacinia quis
-          vel.
+         { children }
         </p>
       </div>
     </Card>
